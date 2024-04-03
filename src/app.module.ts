956f9f8615ef20008bed749c229be285df85d674
Euizzang8001 <a73875323@gmail.com>
2024-04-03 22:05:30 +0900

    20240403

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service'; //다른 모듈이 필요하면 여기에 넣으면 된다.
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 
 @Module({ //controller 등을 총괄함 ex) controller는 Appservice가 없다면 실행이 안되는데 controller에게 Appservice가 있을 때 알려줌
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal:true}),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/entities/post.entity.ts
@@ -0,0 +1,25 @@
+import{
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    ManyToOne,
+}from 'typeorm'
+import { UserEntity } from './user.entity';
+
+@Entity()
+export class PostEntity{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    content: string;
+
+    @ManyToOne(() => UserEntity, {onDelete: 'SET NULL'})
+    author: UserEntity;
+
+    @Column({default: 0})
+    likes: number;
+}
