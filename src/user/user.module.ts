24a284933da54aab1fe3fa8ddcdef7ef1862a926
Euizzang8001 <a73875323@gmail.com>
2024-05-01 20:24:26 +0900

    240501

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import {UserEntity} from 'src/entities';
-import {TypeOrmModule} from '@nestjs/typeorm'
+import { UserEntity } from 'src/entities';
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { UserRepository } from 'src/repositories';
 
 
 @Module({
   imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, UserRepository],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,50 +1,31 @@
-import { HttpException, Injectable } from '@nestjs/common';
-import {InjectRepository} from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
 import {UserEntity} from 'src/entities';
-import {Repository} from 'typeorm';
+import { UserRepository } from 'src/repositories';
 
 @Injectable()
 export class UserService {
-    constructor(
-        @InjectRepository(UserEntity)
-        private readonly userRepository: Repository<UserEntity>,
-    ) {}
+    constructor(private readonly userRepository: UserRepository) {}
 
-    async getUsers(): Promise<UserEntity[]> {
-        const users = this.userRepository.find();
+    async getUsers(): Promise<UserEntity[]>{
+        const users = this.userRepository.findAll();
         return users;
     }
 
-    async findByEmail(email: string): Promise<UserEntity>{
-        const user = this.userRepository.findOne({where: {email}});
+    async findByEmail(email: string): Promise<UserEntity> {
+        const user = this.userRepository.findByEmail(email);
         return user;
     }
 
-    async addUser(info: UserEntity): Promise<UserEntity | void>{
-        if(!(await this.findByEmail(info.email))){
-            const user = this.userRepository.create(info);
-            return await this.userRepository.save(user);
-        }
-        else{
-            throw new HttpException('user already exists', 409)
-        }
+    async addUser(info: UserEntity): Promise<UserEntity>{
+        const newUser = await this.userRepository.create(info);
+        return newUser;
     }
 
     async deleteUser(email: string): Promise<void>{
-        if(await this.findByEmail(email)){
-            await this.userRepository.delete({email});
-        }
-        else{
-            throw new HttpException('user not found', 404)
-        }
+        await this.userRepository.deleteByEmail(email);
     }
 
-    async updatePassword(email: string, password: string): Promise<void>{
-        if(await this.findByEmail(email)){
-            await this.userRepository.update({email}, {password});
-        }
-        else{
-            throw new HttpException('user not found', 404)
-        }
+    async updatePassword(email: string, password: string): Promise<void> {
+        await this.userRepository.updatePassword(email, password);
     }
 }
